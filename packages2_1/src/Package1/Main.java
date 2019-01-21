d7280387bd314b078d4594d01214ccfea4d3270a
DukaArtem <43370038+DukaArtem@users.noreply.github.com>
2019-01-21 17:08:36 +0200

    Add files via upload

---
+++ packages2_1/src/Package1/Main.java
@@ -0,0 +1,14 @@
+package Package1;
+import  Package2.pak1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        pak1 p = new pak1();
+        p.pl();
+        p.PL();
+
+
+    }
+}
---
+++ packages2_1/src/Package2/pak1.java
@@ -0,0 +1,21 @@
+﻿package Package2;
+
+public class pak1 {
+    public int S,S1,n,a1,A1;
+    public int an=6;
+    public void pl() {
+        a1 = 2;
+        n = an - a1 + 1;
+        S = (a1 + an) * n / 2;
+        System.out.println("Первая прогрессия: "+S);
+    }
+    public void PL(){ 
+        A1=3;
+        n = an - A1 + 1;
+        S1 = (A1 + an) * n / 2;
+        System.out.println("Вторая прогрессия: "+S1);
+    }
+
+
+
+}
